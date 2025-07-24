dc10839f3c13d170ccb3b80082e4666ab2ab3d07
roniwellington <roniwellington@gmail.com>
2025-07-24 13:35:05 -0300

    esqueleto do projeto montado

--- livros/urls.py
+++ livros/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from livros.views import index,livros_lidos
 
 urlpatterns = [
-    path('', index),
-    path('livrosLidos/', livros_lidos)
+    path('', index,name='index'),
+    path('livrosLidos/', livros_lidos, name='livrosLidos')
 ]
