36f329d6370f0be6b55e1dfde8b93482e0150e90
roniwellington <roniwellington@gmail.com>
2025-07-24 08:00:17 -0300

    Criando os estilos da pagina index, e adicionando a pagina de livros lidos

--- livros/urls.py
+++ livros/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from livros.views import index
+from livros.views import index,livros_lidos
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('livrosLidos/', livros_lidos)
 ]
--- livros/views.py
+++ livros/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'livros/index.html')
+
+def livros_lidos(request):
+    return render(request, 'livros/livrosLidos.html')
     
 
 # Create your views here.
