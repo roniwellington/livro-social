45c3fe7203a2395065807c10d3fbb38b990855f9
roniwellington <roniwellington@gmail.com>
2025-07-24 22:16:57 -0300

    alterações no admin  e a criação das categorias dos livros no banco de dados

--- livros/admin.py
+++ livros/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from livros.models import Livro
 
-# Register your models here.
+class ListandoLivros(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "descricao", "imagem")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+admin.site.register(Livro, ListandoLivros)
--- livros/models.py
+++ livros/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
 class Livro(models.Model):
+    
+    OPCOES_CATEGORIA = [
+        ("FICÇÃO CIENTÍFICA", "Ficção Científica"),
+        ("FANTASIA", "Fantasia"),
+        ("BIOGRAFIA", "Biografia"),
+        ("NEGÓCIOS E ECONÔMIA", "Negôcios e Econômia"),
+        ("CIÊNCIAS", "Ciências"),
+        ("OUTROS", "Outros"),
+    ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     imagem = models.CharField(max_length=100, null=False, blank=False)
     
