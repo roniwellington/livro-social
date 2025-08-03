c2048d70491c3e7120f14bcc9c8619ec14ee39df
roniwellington <roniwellington@gmail.com>
2025-08-03 07:59:41 -0300

    class criada para a página de formularios

---
+++ usuarios/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label="Nome de Login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={"class": "input-field", "placeholder": "Ex: João Silva"}
+        ),
+    )
+    senha = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={"class": "input-field", "placeholder": "Digite sua senha"}
+        ),
+    )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={"class": "input-field", "placeholder": "Ex: João Silva"}
+        ),
+    )
+    email=forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={"class": "input-field", "placeholder": "joaosilva@xpto.com"}
+        )
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={"class": "input-field", "placeholder": "Digite sua senha"}
+        ),
+    )
+    senha_2 = forms.CharField(
+        label="Confirme sua Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={"class": "input-field", "placeholder": "Digite sua senha novamente"}
+        ),
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroForms
 
-# Create your views here.
+def login(request):
+    form = LoginForms()
+    return render(request, "usuarios/login.html", {"form": form})
+
+def cadastro(request):
+    form = CadastroForms()
+    return render(request, "usuarios/cadastro.html", {"form": form})
